373820f343366c20f2edbc32dc09d0ad9d3f61e9
Bas Paap <paap@kissthefrog.nl>
2024-05-30 11:28:39 +0200

    Add clarifying comments to CallbackCaller

--- src/CallbackCaller.cpp
+++ src/CallbackCaller.cpp
@@ -2,22 +2,27 @@
 
 void Bas::CallbackCaller::begin(simple_callback_t callback)
 {
+    // Store the function pointer for later use.
     this->simpleCallback = callback;
 }
 
 void Bas::CallbackCaller::begin(instance_callback_t callback, instance_pointer_t instance)
 {
+    // Store the function pointer and a pointer to the class instance for later use.
     this->instanceCallback = callback;
     this->instance = instance;
 }
 
 void Bas::CallbackCaller::call()
 {
+    // If the simple function pointer is set, call it.
     if (this->simpleCallback)
     {
         this->simpleCallback();
     }
 
+    // If the pointer to the instance callback and the pointer to the instance are set,
+    // call the callback and pass it the pointer to the instance.
     if (this->instanceCallback && this->instance)
     {
         this->instanceCallback(this->instance);
--- src/CallbackCaller.h
+++ src/CallbackCaller.h
@@ -12,19 +12,19 @@ namespace Bas
     class CallbackCaller
     {
         public:
-        using instance_pointer_t = void*;
         using simple_callback_t = void(*)();
+        using instance_pointer_t = void*;
         using instance_callback_t = void(*)(instance_pointer_t);
 
         private:
-        simple_callback_t simpleCallback;
-        instance_callback_t instanceCallback;
-        instance_pointer_t instance;
+        simple_callback_t simpleCallback;       // Pointer to a global callback function or lambda
+        instance_callback_t instanceCallback; // Pointer to a static callback funciton that will also receive a pointer to the class instance so that it can call a member function.
+        instance_pointer_t instance; // Pointer to the instance of the class the required callback is a member of.
 
         public:
-        void begin(simple_callback_t callback);
-        void begin(instance_callback_t callback, instance_pointer_t instance);
-        void call();
+        void begin(simple_callback_t callback); // Overload for attaching a simple global callback function or lambda.
+        void begin(instance_callback_t callback, instance_pointer_t instance);  // Overload for attaching a static function that will receive an instance of the class that has the callback as a member function.
+        void call(); // Call the attached callbacks
     };
 }
 
